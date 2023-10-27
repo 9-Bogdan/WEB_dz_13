b2a9b9f48ad2224ea8fe446e9fa85a58ffeb6d4e
9-Bogdan <127949078+9-Bogdan@users.noreply.github.com>
2023-10-27 18:45:40 +0300

    ResetPasswordView

--- part_two/users/urls.py
+++ part_two/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
-from .views import RegisterView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
+from .views import RegisterView, ResetPasswordView
 from .forms import LoginForm
 
 app_name = "users"
@@ -9,7 +9,7 @@ urlpatterns = [
     path("signup/", RegisterView.as_view(), name="signup"),
     path("login/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm, redirect_authenticated_user = True), name="login"),
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
-    path('reset-password/',PasswordResetView.as_view(), name='password_reset'),
+    path('reset-password/',ResetPasswordView.as_view(), name='password_reset'),
     path('reset-password/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
     path('reset-password/confirm/<uidb64>/<token>/',
